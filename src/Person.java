b673e1aa961649d33aabd483165da6c19bc351e7
Zoltan Marai <zoltanmarai51@gmail.com>
2020-11-10 15:06:50 +0100

    People class is aavelable.

---
+++ src/Person.java
@@ -0,0 +1,39 @@
+public class Person {
+    private String name;
+    private String city;
+    private String email;
+
+
+    public Person() {
+    }
+
+    public Person(String name, String city, String email) {
+        this.name = name;
+        this.city = city;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
